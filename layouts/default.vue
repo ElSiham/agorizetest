13a6be844ac033ebfd067a80e748d8f6644a46e2
Siham <elhouakmi.siham@gmail.com>
2018-11-30 02:00:03 +0100

    change db + product page

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,22 @@
     <div class="header">
       <div class="shopname">My shop</div>
       <div class="navigation-links">
-        <div class="link">Produits</div>
-        <div class="link">Panier</div>
+        <div class="link-container">
+          <a
+            class="links"
+            href="./product"
+          >
+            Products
+          </a>
+        </div>
+        <div class="link-container">
+          <a
+            class="links"
+            href=""
+          >
+            Cart (0)
+          </a>
+        </div>
       </div>
     </div>
     <nuxt/>
@@ -18,22 +32,24 @@ body
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif
   font-size: 16px
   word-spacing: 1px
-  background-color: #CDD3D5
+  background-color: rgba(240, 240, 240, 1)
   
   .header
-    background-color: #506C64
-    color: #CDD3D5
+    // background-color: #F0F0EC
     .shopname
       float: left
       max-width: 50%
       padding: 1rem
       font-size: 2rem
       font-weight: bold
-      color: #220C10
+      color: rgba(40, 40, 40, 1)
     .navigation-links
       display: flex
       justify-content: flex-end
-      .link
+      .link-container
         padding: 1rem
+        .links
+          color: rgba(104, 104, 104, 1)
+          text-decoration: none
 
 </style>
--- pages/product.vue
+++ pages/product.vue
@@ -1,17 +1,48 @@
 <template>
   <section class="container">
-    <div>
-      {{ product.name }}
+    <div
+      v-for="product in products"
+      :key="product.id"
+      class="product"
+    >
+      <img
+        :src="product.img"
+        alt="product-img"
+      >
+      <div class="product-title">
+        <span>{{ product.name }}</span>
+        <span class="product-price"> {{ product.price }} € </span>
+      </div>
+      <div class="product-descr"> {{ product.descr }} </div>
+      <button class="buy">
+        add to cart
+      </button>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import axios from 'axios'
 
 export default {
-  components: {
-    Logo
+  // components: {
+  // },
+  data () {
+    return {
+      products: []
+    }
+  },
+  asyncData ({ params }) {
+    return axios.get(`http://localhost:3003/products`)
+    .then((res) => {
+      console.log('coucou', res.data)
+      // res.data.playlists.map(item => {
+      //   item.size = item.size.toString()
+      // })
+      return {
+        products: res.data
+      }
+    })
   }
 }
 </script>
@@ -20,9 +51,42 @@ export default {
 
 .container
   min-height: 100vh
-  display: flex
-  justify-content: center
-  align-items: center
-  text-align: center
-
+  padding: 3rem
+  .product
+    position: relative
+    height: 18rem
+    border: 1px solid rgba(200, 200, 200, 1)
+    border-radius: 3px
+    margin-top: 1rem
+    padding: 1rem
+    img
+      max-width: 100%
+      max-height: 100%
+    .product-title
+      color: rgba(40, 40, 40, 1)
+      margin: 1rem 0
+      text-align: center
+      font-weight: bold
+      font-size: 1.3rem
+      .product-price
+        position: absolute
+        right: 2rem
+        top: 2rem
+        font-size: 1rem
+        background-color: rgba(240, 240, 240, 1)
+        padding: .3rem
+        border-radius: 3px
+        color: rgba(0, 137, 123, 1)
+    .product-descr
+      color: rgba(104, 104, 104, 1)
+      text-align: center
+    .buy
+      display: block
+      margin: 1rem 0
+      padding: .5rem
+      border: none
+      border-radius: 3px
+      width: 100%
+      background-color: rgba(191, 54, 12, 1)
+      color: rgba(240, 240, 240, 1)
 </style>
